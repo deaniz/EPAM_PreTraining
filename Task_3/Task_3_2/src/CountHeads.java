f159ae92531ad8e8980177089bd0334c743c3695
deaniz <denischubrick@gmail.com>
2018-05-06 14:35:48 +0300

    Flow chart 3_2 was added, Task_3_2 was improved

--- Task_3/Task_3_2/src/CountHeads.java
+++ Task_3/Task_3_2/src/CountHeads.java
@@ -1,17 +1,20 @@
 public class CountHeads{
     public static final int youngAge = 200;
-    public static final int middleAge = 300;
+    public static final int midAge = 300;
     public static final int youngHeads = 3;
-    public static final int middleHeads = 2;
+    public static final int midHeads = 2;
     public static final int oldHeads = 1;
 
-    public static int countHeads (int age, int initHeads) {
-        if (age <= youngAge) {
+    public static int countHeads (int age, int initHeads){
+        if (age < 0){
+            return 0;
+        }
+        else if (age <= youngAge){
             return initHeads + age * youngHeads;
         }
-        else if (age <= middleAge) {
-            return initHeads + youngAge * youngHeads + (age - youngAge) * middleHeads;
+        else if (age <= midAge){
+            return initHeads + youngAge * youngHeads + (age - youngAge) * midHeads;
         }
-        return initHeads + youngAge * youngHeads + (middleAge - youngAge) * middleHeads + (age - middleAge) * oldHeads;
+        return initHeads + youngAge * youngHeads + (midAge - youngAge) * midHeads + (age - midAge) * oldHeads;
     }
 }
--- Task_3/Task_3_2/src/Main.java
+++ Task_3/Task_3_2/src/Main.java
@@ -1,13 +1,14 @@
 public class Main{
+    public static final int eyesPerHead = 2;
+
     public static void main (String[] args){
         int initHeads = 3; // initial amount of heads
         int age = 654;
 
         int currentHeads = CountHeads.countHeads (age, initHeads);
-        int currentEyes = currentHeads * 2;
+        int currentEyes = currentHeads * eyesPerHead;
 
         System.out.println ("*** Task_3_2 - Heads and eyes of a dragon ***");
-        System.out.println ("Current age: " + age);
         System.out.println ("Current amount of heads: " + currentHeads + "\nCurrent amount of eyes: " + currentEyes);
     }
 }
