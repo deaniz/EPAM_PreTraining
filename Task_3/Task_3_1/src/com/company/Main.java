3d98865811eae362bf3883f3f842b8044d5dc9b2
deaniz <denischubrick@gmail.com>
2018-05-06 13:42:28 +0300

    Flow chart 3_1 was added, task_3_1 was improved

--- Task_3/Task_3_1/src/com/company/Main.java
+++ Task_3/Task_3_1/src/com/company/Main.java
@@ -2,15 +2,18 @@ package com.company;
 
 public class Main{
     public static void main (String[] args){
-        Coordinates a = new Coordinates (2, 2);
-        Coordinates b = new Coordinates (-1, 2);
-        Coordinates c = new Coordinates (-1, -3);
+        int x1 = 2;
+        int y1 = 2;
+        int x2 = -1;
+        int y2 = 2;
+        int x3 = -1;
+        int y3 = -3;
 
         System.out.println ("*** Task_3_1 - Three points (triangle) ***");
-        boolean isTriangle = TriangleCheck.canBeTriangle (a, b, c);
+        boolean isTriangle = TriangleCheck.canBeTriangle (x1, y1, x2, y2, x3, y3);
         if (isTriangle){
             System.out.println ("Can be a triangle");
-            boolean isRightAngled = TriangleCheck.rightAngled (a, b, c);
+            boolean isRightAngled = TriangleCheck.rightAngled (x1, y1, x2, y2, x3, y3);
             if (isRightAngled) {
                 System.out.println ("Can be a right angled triangle");
             }
--- Task_3/Task_3_1/src/com/company/TriangleCheck.java
+++ Task_3/Task_3_1/src/com/company/TriangleCheck.java
@@ -1,34 +1,35 @@
 package com.company;
 
 public class TriangleCheck{
-    public static boolean canBeTriangle (Coordinates a, Coordinates b, Coordinates c){
-        if (a.x == b.x){
-            if (a.x == c.x) {
+    public static boolean canBeTriangle (int x1, int y1, int x2, int y2, int x3, int y3){
+        if (x1 == x2){
+            if (x1 == x3) {
                 return false;
             }
             return true;
         }
-        else if (a.y == b.y) {
-            if (a.y == c.y) {
+        else if (y1 == y2) {
+            if (y1 == y3) {
                 return false;
             }
             return true;
         }
         return true;
     }
-    public static boolean rightAngled (Coordinates a, Coordinates b, Coordinates c){
-        if (a.x == b.x || a.y == b.y){
-            if (a.x == c.x || a.y == c.y){
+    public static boolean rightAngled (int x1, int y1, int x2, int y2, int x3, int y3){
+        if (x1 == x2 || y1 == y2){
+            if (x1 == x3 || y1 == y3){
                 return true;
             }
-            else if (b.x == c.x || b.y == c.y) {
+            else if (x2 == x3 || y2 == y3) {
                 return true;
             }
+            return false;
         }
-        else if (a.y == c.y && b.x == c.x) {
+        else if (y1 == y3 && x2 == x3) {
             return true;
         }
-        else if (a.x == c.x && b.y == c.y) {
+        else if (x1 == x3 && y2 == y3) {
             return true;
         }
         return false;
